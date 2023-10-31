bc84f9abe3189af677aae4f42799477cf4d057d7
Athanasios Nakas <athanani@csd.auth.gr>
2023-10-31 20:52:00 +0200

    Added a script that moves the workspaces in the wanted outputs

---
+++ .config/i3/scripts/outputs.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+i3-msg '[workspace=1]' move workspace to output HDMI-1
+i3-msg '[workspace=2]' move workspace to output HDMI-1
+i3-msg '[workspace=3]' move workspace to output eDP-1
+i3-msg '[workspace=5]' move workspace to output eDP-1
+i3-msg '[workspace=6]' move workspace to output eDP-1
